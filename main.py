8fb8c6df5d78f9cc3e90ee4ffe4989ec41e64584
HanmaMurik <murodbekgaffarov@gmail.com>
2023-08-22 14:41:35 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+def addition(a: int, b: int) ->dict:
+    return {'keyword': a + b}
+
+print(addition(22, 44))
+
+
+def multiplication(a: int, b: int) ->dict:
+    return {'keyword': a * b}
+
+print(multiplication(22, 4))
+
+
+
+def division(a: int, b: int) ->dict:
+    return {'keyword': a / b}
+
+print(division(22, 2))
+
+
+
+
+
+
+
+
+
+
+
